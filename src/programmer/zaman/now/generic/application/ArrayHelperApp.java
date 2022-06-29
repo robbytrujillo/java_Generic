d14097eee25cbf18e10209f649b1c825cbb14378
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-06-29 09:26:36 +0700

    Generic Method

---
+++ src/programmer/zaman/now/generic/application/ArrayHelperApp.java
@@ -0,0 +1,16 @@
+package programmer.zaman.now.generic.application;
+
+import programmer.zaman.now.generic.util.ArrayHelper;
+
+public class ArrayHelperApp {
+    public static void main(String[] args) {
+
+        String[] names = {"Robby", "Ilham", "Kusuma"};
+        Integer[] numbers = {1, 2, 3, 4, 5};
+
+        System.out.println(ArrayHelper.<String>count(names));
+        System.out.println(ArrayHelper.count(numbers));
+
+    }
+
+}
