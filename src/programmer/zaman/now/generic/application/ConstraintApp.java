f9e8d62a40dbbea2ddd47f31ea2fc6ce2119122a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-03 04:20:17 +0700

    Up Bounded Type Parameter

--- src/programmer/zaman/now/generic/application/ConstraintApp.java
+++ src/programmer/zaman/now/generic/application/ConstraintApp.java
@@ -7,11 +7,15 @@ public class ConstraintApp {
     NumberData<Long> longNumberData = new NumberData<>(1L);
 
     //error karena bukan number
-    NumberData<String> stringNumberData = new NumberData<String>(1L);
+    //NumberData<String> stringNumberData = new NumberData<String>(1L);
 
     }
 
-    public static class NumberData<T extends Number> { //jika Number diganti jadi Object maka akan bisa
+    //public static class NumberData<T extends Number> { //jika Number diganti jadi Object maka akan bisa
+    public static class NumberData<T extends Number > { //jika Number & String di Implemntasikan bersama
+        //tapi akan menjadi error karena di java tidak boleh punya parrent > 1 class & hanya boleh Interface dan
+        // yang lain sebagainya
+
         private T data;
 
         public NumberData(T data) {
