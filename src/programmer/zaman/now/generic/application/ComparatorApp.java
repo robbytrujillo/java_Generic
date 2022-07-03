c87cdc024802069a7d79e7f6e555363eacfe9e13
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-07-04 06:31:57 +0700

    Comparator Interface

---
+++ src/programmer/zaman/now/generic/application/ComparatorApp.java
@@ -0,0 +1,28 @@
+package programmer.zaman.now.generic.application;
+
+import programmer.zaman.now.generic.Person;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ComparatorApp {
+    public static void main(String[] args) {
+
+        Person[] people = {
+                new Person("Robby", "Indonesia"),
+                new Person("Ilham", "Indonesia"),
+                new Person("Kusuma", "Indonesia")
+        };
+
+        Comparator<Person> comparator = new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        };
+
+        Arrays.sort(people, comparator);
+
+        System.out.println(Arrays.toString(people));
+    }
+}
